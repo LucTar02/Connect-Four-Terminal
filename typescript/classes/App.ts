58405a79fecea471a25e7f964f6f2c91d108d176
Tarnell <lucas.tarnell@gmail.com>
2024-09-02 20:08:24 +0200

    win horizontal, not vertical, who won.

--- typescript/classes/App.ts
+++ typescript/classes/App.ts
@@ -2,7 +2,7 @@ import prompt from "../helpers/prompt.js";
 import Board from "./Board.js";
 import Player from "./Player.js";
 
-export default class App{
+export default class App {
 
   board: Board;
   playerX: Player;
@@ -12,10 +12,10 @@ export default class App{
     // a while-loop that let us play the game repeatedly
     while (true) {
       this.createPlayers();
-      console.log('fem');
+      console.log('player Created');
       this.board = new Board();
       this.startGameLoop();
-      //this.whoHasWonOnGameOver();
+      this.whoHasWonOnGameOver();
       console.log('');
       let playAgain = prompt('Vill ni spela igen? (ja/nej)? ');
       if (playAgain !== 'ja') { break; }
@@ -23,16 +23,40 @@ export default class App{
   }
 
   createPlayers(): void {
-    console.clear();
+    //console.clear();
     console.log('Connect-Four\n');
     this.playerX = new Player(prompt('Spelare X:s namn: '), 'X', this.board);
     this.playerO = new Player(prompt('Spelare O:s namn: '), 'O', this.board);
   }
 
-  startGameLoop(): void {
-    while (!this.board.gameOver)
-      console.clear();
+    startGameLoop(): void {
+    while (!this.board.gameOver){
+      //console.clear();
+      console.log('game-loop');
     this.board.render();
+    let player = this.board.currentPlayerColor === 'X'
+      ? this.playerX : this.playerO;
+      let move = prompt(
+        `Ange ditt drag ${player.color} ${player.name} - skriv in rad:`
+    );
+    let [column] = move.split(',').map((x: string) => +x.trim() - 1);
+    this.board.makeMove(player.color, column,);
+      }
+    }
+
+
+  whoHasWonOnGameOver(): void {
+    // the game is over, tell the player who has one or if we have a draw
+    //console.clear();
+    console.log('who-won');
+    this.board.render();
+    if (this.board.winner) {
+      let winningPlayer = (this.board.winner === 'X' ? this.playerX : this.playerO);
+      console.log(`Grattis ${winningPlayer.color}: ${winningPlayer.name} du vann!`);
+    }
+    else {
+      console.log('Tyvärr det blev oavgjort...');
+    }
   }
 
 }
--- typescript/classes/Board.ts
+++ typescript/classes/Board.ts
@@ -3,17 +3,18 @@ export default class Board {
   matrix: Array<Array<string>>;
   currentPlayerColor: string;
   gameOver: boolean;
+  isADraw: boolean;
+  winner: string | boolean;
 
-  constructor() {      
-    // 
+  constructor() {
+    
     this.matrix = [...new Array(6)].map(_row =>
       [...new Array(7)].map(_column => ' ')
     );
     // currentPlayer, start with X?
-    this.currentPlayerColor = 'X';
-    
-    //this.winner = false;
-    //this.isADraw = false;
+    this.currentPlayerColor = 'X';    
+    this.winner = false;
+    this.isADraw = false;
     this.gameOver = false;
   }
 
@@ -27,6 +28,84 @@ export default class Board {
           + '|').join(line) +
         line
       );
+  }
+  
+  makeMove(color: string,  column: number): boolean {
+    // don't make any move if the game is over
+    if (this.gameOver) { return false; }
+    // check that the color is X or O - otherwise don't make the move
+    if (color !== 'X' && color !== 'O') { return false; }
+    // check that the color matches the player's turn - otherwise don't make the move
+    if (color !== this.currentPlayerColor) { return false; }
+    // check that the row and column are numbers - otherwise don't make the move
+    //if (isNaN(row) || isNaN(column)) { return false; }
+    if (isNaN(column)) { return false; }
+    
+    // check that the row is between 0 and 2 - otherwise don't make the move
+    //if (this.matrix[row][column] !== ' ') { return false; }
+    
+    //if (row < 0 || row >= this.matrix.length) { return false; }
+
+    // check that the column is between 0 and matrix[0] - otherwise don't make the move
+    if (column < 0 || column >= this.matrix[0].length) { return false; }
+    // check that the position is empty - otherwise don't make the move
+        let row = -1;
+    for (let r = this.matrix.length - 1; r >= 0; r--) {
+        if (this.matrix[r][column] === ' ') {
+            row = r;
+            break;
+        }
     }
+    // If no empty row is found, the column is full
+    if (row === -1) { return false; }
+    
+    // make the move
+    this.matrix[row][column] = color;
+    // change the current player color
+    this.currentPlayerColor = this.currentPlayerColor === 'X' ? 'O' : 'X';
+    // check if someone has won or if it's a draw/tie and update properties
+    this.winner = this.winCheck();
+    this.isADraw = this.drawCheck();
+    // the game is over if someone has won or if it's a draw
+    this.gameOver = !!(this.winner || this.isADraw);
+    // return true if the move could be made
+    return true;
+  }
+
+
+  winCheck(): string | false {
+    console.log('test')
+    let m = this.matrix;
+        for (let color of 'XO') {
+          // r = row, c = column
+          
+              let offsets = [
+      [[0, 0], [0, 1], [0, 2],[0, 3]],  // horizontal win
+      [[0, 0], [1, 0], [2, 0],[0, 3]],  // vertical win
+      [[0, 0], [1, 1], [2, 2],[3, 3]],  // vänster upp till höger nere
+      [[0, 0], [1, -1], [2, -2],[3, -3]] // höger upp till vänster nere
+          ];
+          
+      for (let r = 0; r < m.length; r++) {
+        for (let c = 0; c < m[0].length; c++) {
+          // ro = row offset, co = column offset
+          for (let winType of offsets) {
+            let colorsInCombo = '';
+            for (let [ro, co] of winType) {
+              colorsInCombo += (m[r + ro] || [])[c + co];
+            }
+            if (colorsInCombo === color.repeat(4)) {
+              return color;
+            }
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  drawCheck(): boolean {
+    return !this.winCheck() && !this.matrix.flat().includes(' ');
+  }
 
 }
